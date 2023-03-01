eba7a197d99fd39168b19f865abf5dcc033f6c6b
PrgDominique <dominiquesimbillo10@gmail.com>
2023-03-01 14:30:28 +0800

    added register

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,24 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/User'
+
+// Register
+
+export const register = async(req, res){
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body
+
+       
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
